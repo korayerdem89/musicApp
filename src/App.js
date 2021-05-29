34e19bf3cd62e3980be1835f478e1ba22de6a416
Koray Erdem <developerkoray@gmail.com>
2021-05-29 22:57:02 +0300

    added JSON file and printed the album list into the screen

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
 
-import {SafeAreaView, StyleSheet} from 'react-native';
+import {SafeAreaView, Text, View, StyleSheet, FlatList, Dimensions} from 'react-native';
+import music_data from './music_data.json';
 
 const App = () => {
-  return <SafeAreaView></SafeAreaView>;
+  return (
+    <SafeAreaView style={styles.container}>
+      <View>
+        <FlatList
+          data={music_data}
+          renderItem={({item}) => <Text>{item.title}</Text>}
+        />
+      </View>
+    </SafeAreaView>
+  );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {flex:1},
+});
 
 export default App;
