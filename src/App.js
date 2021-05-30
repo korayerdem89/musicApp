00d424a64b3a46a39eb1cdd76e39d15faf4a955c
Koray Erdem <developerkoray@gmail.com>
2021-05-30 18:22:26 +0300

    main style has finished

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
 
-import {SafeAreaView, Text, View, StyleSheet, FlatList, Dimensions} from 'react-native';
+import {SafeAreaView, View, StyleSheet, FlatList} from 'react-native';
 import music_data from './music_data.json';
+import SongCard from './components/SongCard';
 
 const App = () => {
+  const renderSong = ({item}) => <SongCard song={item} />;
+
+  const renderSeparator = () => <View style={styles.separator} />;
   return (
     <SafeAreaView style={styles.container}>
-      <View>
+      <View style={styles.container}>
         <FlatList
+          keyExtractor={item => item.id}
           data={music_data}
-          renderItem={({item}) => <Text>{item.title}</Text>}
+          renderItem={renderSong}
+          ItemSeparatorComponent={renderSeparator} 
         />
       </View>
     </SafeAreaView>
@@ -17,7 +23,9 @@ const App = () => {
 };
 
 const styles = StyleSheet.create({
-  container: {flex:1},
+  container: {flex: 1},
+  separator: {borderWidth: 1,
+  borderColor: '#e0e0e0',}
 });
 
 export default App;
---
+++ src/components/SongCard/SongCard.styles.js
@@ -0,0 +1,18 @@
+import {StyleSheet} from 'react-native';
+export default StyleSheet.create({
+  container: {
+    padding: 10,
+    flexDirection: 'row',
+  },
+  image: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+  },
+  inner_container: {padding: 10, flex:1, justifyContent:'center',},
+  title: {fontWeight: 'bold', fontSize: 27},
+  info_container: {
+    flexDirection: 'row',
+  },
+  year: { marginLeft:10, color:'gray', fontWeight:'bold',},
+});
