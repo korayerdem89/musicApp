3dc0cd3caf3625464476f15e5bc3d37a1cf12165
Koray Erdem <developerkoray@gmail.com>
2021-05-30 20:37:04 +0300

    onChangeText proplu SearchBar componenti eklendi

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import React from 'react';
 import {SafeAreaView, View, StyleSheet, FlatList} from 'react-native';
 import music_data from './music_data.json';
 import SongCard from './components/SongCard';
+import SearchBar from './components/SearchBar';
 
 const App = () => {
   const renderSong = ({item}) => <SongCard song={item} />;
 
   const renderSeparator = () => <View style={styles.separator} />;
+
+  const handleSearch = text => console.log(text);
+
   return (
     <SafeAreaView style={styles.container}>
+      <SearchBar onSearch={handleSearch} />
       <View style={styles.container}>
         <FlatList
           keyExtractor={item => item.id}
